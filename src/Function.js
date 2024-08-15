12aa1280f16e6b86726f3d15d905d2092d29c778
Jack Holden <Jack.Holden@Vitality.co.uk>
2024-08-15 14:21:54 +0100

    deprecate: Deprecate function Y ready for removal in next major bump

--- src/Function.js
+++ src/Function.js
@@ -4,3 +4,7 @@ i am docs
 export const sayHello = () => {
   console.log("hello version made a change jdbwdbqdjwqb jskdbskjf");
 };
+
+export const deprecated = () => {
+  console.log("This is deprecated");
+};
